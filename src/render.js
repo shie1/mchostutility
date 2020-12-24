46d88fa6313fc521a8df82bb0f091943f0be86ba
shie1 <sonkolybence@outlook.hu>
2020-12-24 01:55:30 +0100

    Update

--- src/render.js
+++ src/render.js
@@ -43,7 +43,7 @@ async function host() {
     }
     writeStorage('server', server)
     require('./genfiles.js')
-    let ip = await ngrok.connect({ addr: 25565, authtoken: storage.ngrokAuth })
+    let ip = await ngrok.connect({ proto: 'tcp', addr: 25565, authtoken: storage.ngrokAuth })
     setTimeout(() => {
         serverProcess = cp.exec(`start cmd /k "echo Server ip: ${ip} && java -Xms1024M -Xmx1024M -jar server.jar nogui"`, { cwd: path.resolve('./src/server') });
         serverProcess.on('close', () => {
